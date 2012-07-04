3befde185c58d6de637aad53dca1ec109adb0379
Jim Kinsey <kinjacono@yahoo.co.uk>
2012-07-04 22:58:50 +0100

    Fix test for problem 3 to catch bug with isPrime returning true for 4

--- Java/Problem3-Primes/src/kinsey/jim/euler/Problem3Test.java
+++ Java/Problem3-Primes/src/kinsey/jim/euler/Problem3Test.java
@@ -2,6 +2,8 @@ package kinsey.jim.euler;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.Test;
 
 public class Problem3Test {
@@ -23,20 +25,55 @@ public class Problem3Test {
 	
 	@Test
 	public void test_primeFactors_2() {
-		assertEquals(1, Problem3.primeFactors(2).size());
-		assertEquals(2, Problem3.primeFactors(2).get(0).longValue());
+		List<Long> factors = Problem3.primeFactors(2);
+		
+		assertEquals(1, factors.size());
+		assertEquals(2, factors.get(0).longValue());
 	}
 
 	@Test
 	public void test_primeFactors_3() {
-		assertEquals(1, Problem3.primeFactors(3).size());
-		assertEquals(3, Problem3.primeFactors(3).get(0).longValue());
+		List<Long> factors = Problem3.primeFactors(3);
+		
+		assertEquals(1, factors.size());
+		assertEquals(3, factors.get(0).longValue());
 	}
 
 	@Test
+	public void test_primeFactors_4() {
+		List<Long> factors = Problem3.primeFactors(4);
+		
+		assertEquals(2, factors.size());
+		assertEquals(2, factors.get(0).longValue());
+		assertEquals(2, factors.get(1).longValue());
+	}
+	
+	@Test
 	public void test_primeFactors_6() {
-		assertEquals(2, Problem3.primeFactors(6).size());
-		assertEquals(2, Problem3.primeFactors(6).get(0).longValue());
-		assertEquals(3, Problem3.primeFactors(6).get(1).longValue());
+		List<Long> factors = Problem3.primeFactors(6);
+		
+		assertEquals(2, factors.size());
+		assertEquals(2, factors.get(0).longValue());
+		assertEquals(3, factors.get(1).longValue());
+	}
+	
+
+	@Test
+	public void test_primeFactors_8() {
+		List<Long> factors = Problem3.primeFactors(8);
+		
+		assertEquals(3, factors.size());
+		assertEquals(2, factors.get(0).longValue());
+		assertEquals(2, factors.get(1).longValue());
+		assertEquals(2, factors.get(2).longValue());
+	}
+	
+	@Test
+	public void test_primeFactors_10() {
+		List<Long> factors = Problem3.primeFactors(10);
+		
+		assertEquals(2, factors.size());
+		assertEquals(2, factors.get(0).longValue());
+		assertEquals(5, factors.get(1).longValue());
 	}
 }
