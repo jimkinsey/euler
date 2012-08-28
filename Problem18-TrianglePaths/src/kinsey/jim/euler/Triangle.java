11590b3ddf5fb2db6d5aabcc5c1e51571a89fc32
Jim Kinsey <kinjacono@yahoo.co.uk>
2012-08-28 21:54:31 +0100

    Working solution to problem 18

--- Problem18-TrianglePaths/src/kinsey/jim/euler/Triangle.java
+++ Problem18-TrianglePaths/src/kinsey/jim/euler/Triangle.java
@@ -6,17 +6,21 @@ import java.util.List;
 public class Triangle {
 	private List<List<Integer>> rows = new ArrayList<List<Integer>>();
 	
-	public Triangle(String triangleData) {
-		if (triangleData.isEmpty())
+	public Triangle(String triangleDefinition) {
+		if (triangleDefinition.isEmpty())
 			throw new IllegalArgumentException();
-		rows = parseData(triangleData);
+		rows = parseDefinition(triangleDefinition);
+	}
+	
+	private Triangle(List<List<Integer>> rows) {
+		this.rows = rows;
 	}
 
-	private List<List<Integer>> parseData(String triangleDefinition) {
+	private List<List<Integer>> parseDefinition(String triangleDefinition) {
 		List<List<Integer>> rows = new ArrayList<List<Integer>>();
 		String[] rowDefinitions = triangleDefinition.split("\n");
-		for (String row : rowDefinitions)
-			rows.add(parseRow(row));
+		for (String rowDefinition : rowDefinitions)
+			rows.add(parseRow(rowDefinition));
 		return rows;
 	}
 
@@ -30,5 +34,42 @@ public class Triangle {
 		
 	public int get(int row, int column) {
 		return rows.get(row).get(column);
+	}
+
+	public long maxPathValue() {
+		if (rows.size() == 1)
+			return get(0,0);
+		long max = get(0,0);
+		max += Math.max(subTriangle(1,0).maxPathValue(), subTriangle(1,1).maxPathValue());
+		return max;
+	}
+	
+	public Triangle subTriangle(int row, int column) {
+		if (row == 0)
+			return this;
+		List<List<Integer>> subTriangleRows = new ArrayList<List<Integer>>();
+		for (int i = row; i < rows.size(); i++)
+			subTriangleRows.add(rows.get(i).subList(column, (i - row) + 1 + column));
+		return new Triangle(subTriangleRows);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Triangle)
+			return ((Triangle) obj).rows.equals(this.rows);
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return 23 + (17 * rows.hashCode());
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder toStringBuilder = new StringBuilder();
+		for (List<Integer> row : rows)
+			toStringBuilder.append(row);
+		return toStringBuilder.toString();
 	}
 }
--- Problem18-TrianglePaths/src/kinsey/jim/euler/TriangleTest.java
+++ Problem18-TrianglePaths/src/kinsey/jim/euler/TriangleTest.java
@@ -29,4 +29,47 @@ public class TriangleTest {
 		assertEquals(3, triangle.get(1,1));
 	}
 	
+	@Test
+	public void max_path_value_of_triangle_with_one_row() {
+		Triangle triangle = new Triangle("01");
+		assertEquals(1, triangle.maxPathValue());
+	}
+	
+	@Test
+	public void max_path_value_of_triangle_with_two_rows() {
+		Triangle triangle = new Triangle("01\n02 03");
+		assertEquals(4, triangle.maxPathValue());
+	}
+	
+	@Test
+	public void max_path_value_of_triangle_with_three_rows() {
+		Triangle triangle = new Triangle("01\n02 03\n04 05 06");
+		assertEquals(10, triangle.maxPathValue());
+	}
+	
+	@Test
+	public void max_path_value_using_example_data() {
+		Triangle triangle = new Triangle("3\n7 4\n2 4 6\n8 5 9 3");
+		assertEquals(23, triangle.maxPathValue());
+	}
+	
+	@Test
+	public void sub_triangle_at_0_0_is_same_triangle() {
+		Triangle triangle = new Triangle("01\n02 03");
+		assertEquals(triangle, triangle.subTriangle(0, 0));
+	}
+	
+	@Test
+	public void sub_triangle_with_one_row() {
+		Triangle triangle = new Triangle("01\n02 03");
+		assertEquals(new Triangle("02"), triangle.subTriangle(1, 0));
+	}
+	
+	@Test
+	public void sub_triangle_with_two_rows() {
+		Triangle triangle = new Triangle("01\n02 03\n04 05 06");
+		assertEquals(new Triangle("02\n04 05"), triangle.subTriangle(1, 0));
+		assertEquals(new Triangle("03\n05 06"), triangle.subTriangle(1, 1));
+	}
+	
 }
